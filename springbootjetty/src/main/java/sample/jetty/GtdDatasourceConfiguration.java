011ec762add48f2b409b19150ae9c2097012620e
philip e carter <philip.e.carter@gmail.com>
2016-07-28 18:15:02 -0500

    Fixed header links; failed attempt to connect to AWS db.

--- springbootjetty/src/main/java/sample/jetty/GtdDatasourceConfiguration.java
+++ springbootjetty/src/main/java/sample/jetty/GtdDatasourceConfiguration.java
@@ -18,6 +18,12 @@ public class GtdDatasourceConfiguration {
         dataSource.setUsername("root");
         dataSource.setPassword("");
 
+//      aws attempt throws "communications link failure", both here and from intellij database view.
+//        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+//        dataSource.setUrl("jdbc:mysql://gtd-db.ckqylf4efsrq.us-east-1.rds.amazonaws.com:3306/gtd-db");
+//        dataSource.setUsername("gtd");
+//        dataSource.setPassword("66dinosaur");
+
         return dataSource;
     }
 
